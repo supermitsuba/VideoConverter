b95ddb22618719ef4e5f78bc225b761fe73cde70
supermitsuba <supermitsuba@gmail.com>
2020-09-20 09:09:56 -0400

    logic to map all video and audio

--- convert.sh
+++ convert.sh
@@ -34,10 +34,10 @@ find "${source}" -type f -name "${extension}" -not -path "*/.*/*" -not -path "*/
 	# Convert ts file to mp4
 	if [ "$useh265" = "true" ]; then
   		echo "${DATE_WITH_TIME}: Starting with this file as h265: $sourceFile to $destFile" >> "${logFile}"
-		ffmpeg -nostdin -i "${sourceFile}" -c:v libx265 -c:a aac "${tempFile}" 
+		ffmpeg -nostdin -i "${sourceFile}" -c:v libx265 -c:a aac -map 0:a -map 0:v "${tempFile}" 
 	else
   		echo "${DATE_WITH_TIME}: Starting with this file as h264: $sourceFile to $destFile" >> "${logFile}"
-		ffmpeg -nostdin -i "${sourceFile}" -c:v libx264 -c:a aac "${tempFile}" 
+		ffmpeg -nostdin -i "${sourceFile}" -c:v libx264 -c:a aac -map 0:a -map 0:v "${tempFile}" 
 	fi
 
 	# Move ts file to temp folder
